32e230fd7aa66491bb6a8f7855a592b5859e7c78
Tanmayb25 <tanmay.22111091@viit.ac.in>
2023-08-03 17:23:38 +0530

    start front end

---
+++ src/Pages/Add.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+
+function addparameter(){
+    
+}
+
+function Add() {
+  return (
+    <div>
+      <h3>Product name:</h3>
+      <input></input>
+      <button onClick={addparameter}>Add parameter</button>
+      <button>Save</button>
+
+      <Table striped="columns">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>max</th>
+          <th>min</th>
+          <th>unit</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Length</td>
+          <td>10</td>
+          <td>20</td>
+          <td>cm</td>
+        </tr>
+      </tbody>
+    </Table>
+
+    </div>
+  )
+}
+
+export default Add
